512daf564671d7006726a67277dac0af708b2cc5
AlexSart083 <100191834+AlexSart083@users.noreply.github.com>
2025-08-21 21:27:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -463,7 +463,8 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
-    main()import streamlit as st
+    main()
+import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
